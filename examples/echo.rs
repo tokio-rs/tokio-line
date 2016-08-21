029f56ccbadb9cce6f73149733dcb175aad1c6f5
Holger Rapp <HolgerRapp@gmx.net>
2016-08-21 14:33:59 +0200

    Simplify and document aggressively.
    - Removes Client and Server and replaces them through flat functions
      that are easier to understand.
    - Make reactor creation explicit to understand data flow in Tokio better
      while reading the example.
    - Document the s*** out of the code to make this a literal code walk
      that gets some of the core concepts of Tokio across in an easier to
      understand way.

--- examples/echo.rs
+++ examples/echo.rs
@@ -4,33 +4,48 @@ extern crate tokio_line as line;
 extern crate env_logger;
 
 use futures::Future;
+use tokio::reactor::{Reactor};
 use tokio::Service;
 
 pub fn main() {
     env_logger::init().unwrap();
 
-    let addr = "127.0.0.1:12345".parse().unwrap();
+    // First thing we need is a Tokio reactor which watches all our sources (e.g. sockets,
+    // channels) for readiness and then makes sure our code is actually only called when there is
+    // something to do.
+    let reactor = Reactor::default().unwrap();
 
-    line::Server::new()
-        .bind(addr)
-        .serve(tokio::simple_service(|msg| {
+    // To actually give over control flow to the reactor, we call reactor.spawn() later. Tokio
+    // takes control of our program then, everything is asynchronous from there. To still
+    // communicate with the reactor we need a handle which we can clone at will.
+    let handle = reactor.handle();
+    reactor.spawn();
+
+    // This brings up our server. 
+    let addr = "127.0.0.1:12345".parse().unwrap();
+    line::serve(
+        handle.clone(),
+        addr, 
+        tokio::simple_service(|msg| {
             println!("GOT: {:?}", msg);
             Ok(msg)
         }))
         .unwrap();
 
+    // Now our client. We use the same reactor as for the server - usually though this would be
+    // done in a separate program most likely on a separate machine. 
+    let client = line::connect(handle, &addr).unwrap();
 
-    let client = line::Client::new()
-        .connect(&addr)
-        .unwrap();
-
+    // The connect call returns us a ClientHandle that allows us to use the 'Service' as a function
+    // - one that returns a future that we can 'await' on. 
     let resp = client.call("Hello".to_string());
     println!("RESPONSE: {:?}", await(resp));
 
     drop(client);
 }
 
-// Why this isn't in futures-rs, I do not know...
+// Blocks the execution of the current thread until the future is available. Why this isn't in
+// futures-rs, I do not know... 
 fn await<T: Future>(f: T) -> Result<T::Item, T::Error> {
     use std::sync::mpsc;
     let (tx, rx) = mpsc::channel();
